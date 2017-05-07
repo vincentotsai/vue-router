402e7e7f5520831b408390ec4b6b6491e3e2f6cd
vincentotsai <totsai@139.com>
2017-05-07 21:00:54 +0800

    vuex

---
+++ src/router/routers.js
@@ -0,0 +1,17 @@
+import Main from '../components/Main'
+import Login from '../components/Login'
+
+export default [
+		{
+			path: '/login',
+			component: Login
+		},
+		{
+			path: '/main', 
+			meta: {
+				//meta元素
+				 requireAuth: true,  
+				},           
+				component: Main,
+			}
+]
